100ad1293f01aa9c1f8bb765cbc728a879e22f99
Anish Pallati <i@anish.land>
2025-11-28 01:27:53 -0500

    fix: ensure permissions stay correct

--- modules/common.nix
+++ modules/common.nix
@@ -22,5 +22,13 @@
 
   # PAM Kerberos integration
   security.pam.services.sshd.makeHomeDir = true;
+
+  # Maintain /etc/nixos permissions for shared access
+  system.activationScripts.etcNixosPermissions = ''
+    if [ -d /etc/nixos ]; then
+      chgrp -R wheel /etc/nixos
+      chmod -R g+w /etc/nixos
+    fi
+  '';
 }
 
