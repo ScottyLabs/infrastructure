3a22deede0cf577e325e678b9777920e91ee47bc
Anish Pallati <i@anish.land>
2025-12-10 10:42:42 -0500

    fix: mount top-level btrfs for btrbk to access subvolumes

--- common/btrfs.nix
+++ common/btrfs.nix
@@ -3,6 +3,13 @@
 {
   environment.systemPackages = [ pkgs.btrbk ];
 
+  # Mount top-level btrfs for btrbk to access subvolumes
+  fileSystems."/mnt/btrfs-root" = {
+    device = "/dev/disk/by-partlabel/disk-main-root";
+    fsType = "btrfs";
+    options = [ "subvolid=5" "noatime" ];
+  };
+
   services.btrfs.autoScrub = {
     enable = true;
     interval = "monthly";
@@ -14,7 +21,7 @@
     settings = {
       snapshot_preserve_min = "2d";          # Always keep at least 2 days
       snapshot_preserve = "7d 4w";           # Keep 7 daily + 4 weekly snapshots
-      volume."/" = {
+      volume."/mnt/btrfs-root" = {
         snapshot_dir = ".snapshots";         # Store snapshots in /.snapshots
         subvolume."@".snapshot_create = "always";      # Snapshot root
         subvolume."@home".snapshot_create = "always";  # Snapshot home
