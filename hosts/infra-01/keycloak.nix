d943b217bf92b434f3e6f8b071dab2f8d24d2c41
Anish Pallati <i@anish.land>
2025-12-12 11:46:47 -0500

    feat: add sha256 for rememberMe plugin

--- hosts/infra-01/keycloak.nix
+++ hosts/infra-01/keycloak.nix
@@ -3,7 +3,7 @@
 let
   rememberMe = pkgs.fetchurl {
     url = "https://github.com/Herdo/keycloak-remember-me-authenticator/releases/download/v1.0.0/keycloak-remember-me-authenticator-1.0.0.jar";
-    sha256 = "";
+    sha256 = "sha256-C2SwwFZ9Z8BqczJx8Dx/jnvj8nApncOkyejQz0m+6eA=";
   };
   discord = pkgs.fetchurl {
     url = "https://github.com/wadahiro/keycloak-discord/releases/download/v0.6.1/keycloak-discord-0.6.1.jar";
