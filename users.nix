400a21821981b4acf3eade788d091329ebdd18fe
Anish Pallati <i@anish.land>
2025-12-13 16:40:45 -0500

    feat: add max wen

--- users.nix
+++ users.nix
@@ -13,4 +13,11 @@
     };
     sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdJH1Tg/yryBKrmCgtJk02cfj6+OBIqfPHLuKQvtfyu oojefferywm@protonmail.com";
   };
+  maxwen = {
+    git = {
+      name = "Max Wen";
+      email = "55125103+TenType@users.noreply.github.com";
+    };
+    sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICyfRV5/CqqyG6L7K+td/hw3Xl40Qt+yiXk8u4hMTnXi 55125103+TenType@users.noreply.github.com";
+  };
 }
