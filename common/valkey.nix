1dfc370ca2f0985dd0742085814fe38fb41dfa6c
Anish Pallati <i@anish.land>
2025-12-18 15:51:37 -0500

    feat: add scottylabs.valkey module with automatic socket permissions

--- common/valkey.nix
+++ common/valkey.nix
@@ -8,7 +8,13 @@ in
     servers = lib.mkOption {
       type = lib.types.listOf lib.types.str;
       default = [];
-      description = "List of Valkey server instances to create";
+      description = ''
+        List of Valkey server instances to create.
+        
+        Each server gets a Unix socket at /run/redis-<name>/redis.sock.
+        A system user with the same name is automatically added to the
+        redis-<name> group for socket access.
+      '';
       example = [ "discord-verify" ];
     };
   };
@@ -24,5 +30,12 @@ in
         };
       }) cfg.servers);
     };
+
+    users.users = lib.listToAttrs (map (name: {
+      inherit name;
+      value = {
+        extraGroups = [ "redis-${name}" ];
+      };
+    }) cfg.servers);
   };
 }
