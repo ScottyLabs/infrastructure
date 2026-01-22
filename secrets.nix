ca9e1da130e84e2e59342cdde81fbc4b41917a66
Jeffery Oo <oojefferywm@proton.me>
2026-01-22 00:21:11 -0500

    feat: added secret for openbao

--- secrets.nix
+++ secrets.nix
@@ -17,6 +17,7 @@ in
   "secrets/infra-01/minecraft.age".publicKeys = admins ++ [ infra-01 ];
   "secrets/infra-01/keycloak.age".publicKeys = admins ++ [ infra-01 ];
   "secrets/infra-01/vaultwarden.age".publicKeys = admins ++ [ infra-01 ];
+  "secrets/infra-01/openbao.age".publicKeys = admins ++ [ infra-01 ];
 
   "secrets/prod-01/dalmatian.age".publicKeys = admins ++ [ prod-01 ];
   "secrets/prod-01/discord-verify.age".publicKeys = admins ++ [ prod-01 ];
