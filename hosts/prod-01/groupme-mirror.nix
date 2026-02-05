85038bd838293e9a34e32a5b72d2b094d5d4d06e
Anish Pallati <i@anish.land>
2026-02-05 17:15:22 -0500

    fix: create a static user for groupme-mirror

--- hosts/prod-01/groupme-mirror.nix
+++ hosts/prod-01/groupme-mirror.nix
@@ -21,5 +21,18 @@
   systemd.services.groupme-mirror = {
     after = [ "bao-agent.service" ];
     wants = [ "bao-agent.service" ];
+
+    # Create a static user because groupme-mirror uses a dynamic one
+    serviceConfig = {
+      DynamicUser = lib.mkForce false;
+      User = "groupme-mirror";
+      Group = "groupme-mirror";
+    };
   };
+
+  users.users.groupme-mirror = {
+    isSystemUser = true;
+    group = "groupme-mirror";
+  };
+  users.groups.groupme-mirror = { };
 }
