29371f5d477041e81c8927a5870fc1f2c1363b2b
Anish Pallati <i@anish.land>
2026-03-30 18:52:43 -0400

    fix: forward headers in nginx for S3 signature verification

--- common/garage.nix
+++ common/garage.nix
@@ -76,6 +76,13 @@ in
         forceSSL = true;
         locations."/" = {
           proxyPass = "http://localhost:${toString cfg.s3Port}";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            client_max_body_size 100M;
+          '';
         };
       };
     };
