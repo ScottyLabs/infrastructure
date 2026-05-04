b3c7abd1065710eef56fe2c575e7ef62fa737b5e
Anish Pallati <i@anish.land>
2026-05-03 22:18:37 -0400

    fix: pin caddy-security plugin hash

--- common/garage.nix
+++ common/garage.nix
@@ -30,7 +30,7 @@ let
 
   caddyWithSecurity = pkgs.caddy.withPlugins {
     plugins = [ "github.com/greenpau/caddy-security@v1.1.62" ];
-    hash = lib.fakeHash;
+    hash = "sha256-NpVNGD8y9yW69/i5dXDuN6yuyIe37KHsrMbt7g5povk=";
   };
 in
 {
