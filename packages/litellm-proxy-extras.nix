1f014c4ac16cc783491c02b1e202ec36845d2c54
Anish Pallati <i@anish.land>
2026-05-20 06:36:37 -0700

    fix(litellm-proxy-extras): fetch wheel by content-addressed URL

--- packages/litellm-proxy-extras.nix
+++ packages/litellm-proxy-extras.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchurl,
 }:
 
 # `litellm-proxy-extras` ships the Prisma schema and SQL migrations the
@@ -13,10 +13,8 @@ buildPythonPackage rec {
   version = "0.4.56";
   format = "wheel";
 
-  src = fetchPypi {
-    pname = "litellm_proxy_extras";
-    inherit version format;
-    python = "py3";
+  src = fetchurl {
+    url = "https://files.pythonhosted.org/packages/7a/91/b54c96cfd1f0fabeca686bf400cb7d7c3c12f15df4359d42deefd0fc2bb2/litellm_proxy_extras-0.4.56-py3-none-any.whl";
     hash = "sha256-UtvjtTWMeQ534S8exe+OdQizg8Kq9BKZdQtvtACQjuc=";
   };
 
