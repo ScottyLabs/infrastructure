d9b2ef7f82337b75ff2d09a225cdf7387da44f10
Anish Pallati <i@anish.land>
2026-07-18 01:06:51 -0400

    fix: remove nh and stale aliases
    Signed-off-by: Anish Pallati <i@anish.land>

--- common/base.nix
+++ common/base.nix
@@ -32,21 +32,6 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console.keyMap = "us";
 
-  # nh and garbage collection
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.dates = "daily";
-    clean.extraArgs = "--keep-since 7d --keep 1";
-    flake = "/etc/nixos";
-  };
-
-  # Add alias for nixos-rebuild switch using nh
-  environment.shellAliases = {
-    update = "sudo btrbk run && nh os switch";
-    rollback = "nh os switch --rollback";
-  };
-
   # Nix
   nix.package = pkgs.lixPackageSets.stable.lix;
 
